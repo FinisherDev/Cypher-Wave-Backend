efd7462bba55d66e6601695e5dc39707ee17ca72
FinisherDev <molokwujude002@gmail.com>
2024-11-02 11:56:17 +0100

    Change is inevitable

--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.core.mail import send_mail
 from rest_framework import viewsets
 from .serializers import ContactSerializer
 from .models import Contact
@@ -8,3 +9,8 @@ from .models import Contact
 class ContactView(viewsets.ModelViewSet):
     serializer_class = ContactSerializer
     queryset = Contact.objects.all()
+
+    def perform_create(self, serializer):
+        msg = 'Message From: ' + self.request.data['email'] + '\n Details: ' + self.request.data['message']
+        send_mail("Customer Message", message=msg ,from_email=settings.EMAIL_HOST_USER, recipient_list=['Cypherwave66@gmail.com'])
+        return super().perform_create(serializer)
--- cypherwave/settings.py
+++ cypherwave/settings.py
@@ -129,3 +129,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_SSL = True
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'molokwujude002@gmail.com'
+EMAIL_HOST_PASSWORD = 'hhhm wlel icei ewpp'
